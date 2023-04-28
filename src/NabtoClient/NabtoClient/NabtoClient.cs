52751d682e7d3918a262ac53bddf3c24f6d3d2fa
Ulrik Gammelby <ug@nabto.com>
2023-04-28 17:01:11 +0200

    reformatted docs for NabtoClient

--- src/NabtoClient/NabtoClient/NabtoClient.cs
+++ src/NabtoClient/NabtoClient/NabtoClient.cs
@@ -7,11 +7,13 @@ namespace Nabto.Edge.Client;
 
 /**
  * <summary>
- * This interface is the main entry point for the Nabto Edge Client SDK .Net wrapper.
  *
- * It enables you to create a connection object, used to connect to a Nabto Edge Embedded device. And it provides misc
+ * <para>This interface is the main entry point for the Nabto Edge Client SDK .Net wrapper.</para>
+ *
+ * <para>It enables you to create a connection object, used to connect to a Nabto Edge Embedded device. And it provides misc
  * support functions: Create a private key (mandatory to later connect to a device), control logging, get SDK version.
- * The Client object must be kept alive for the duration of all connections created from it.
+ * The Client object must be kept alive for the duration of all connections created from it.</para>
+ *
  * </summary>
  */
 public interface NabtoClient {
@@ -28,7 +30,6 @@ public interface NabtoClient {
     /**
      * <summary>
      * Get the underlying SDK version.
-     *
      * </summary>
      * <returns>
      *     the SDK version, e.g. 5.2.0
@@ -38,10 +39,11 @@ public interface NabtoClient {
 
     /**
      * <summary>
-     * Create a private key and return the private key as a pem encoded string.
      *
-     * The result is normally stored in a device specific secure location and retrieved whenever a new connection
-     * is established, passed on to a Connection object using `setPrivateKey()`.
+     * <para>Create a private key and return the private key as a pem encoded string.</para>
+     *
+     * <para>The result is normally stored in a device specific secure location and retrieved whenever a new connection
+     * is established, passed on to a Connection object using `setPrivateKey()`.</para>
      *
      * </summary>
      * <exception cref="NabtoException">FAILED if key could not be created</exception>
@@ -53,9 +55,10 @@ public interface NabtoClient {
 
     /**
      * <summary>
-     * Create a connection object.
      *
-     * The created connection can then be configured and opened. Returned object must be kept alive while in use.
+     * <para>Create a connection object.</para>
+     *
+     * <para>The created connection can then be configured and opened. Returned object must be kept alive while in use.</para>
      *
      * </summary>
      * <exception cref="NabtoException">ALLOCATION_ERROR if the underlying SDK fails creating a connection object</exception>
@@ -65,7 +68,6 @@ public interface NabtoClient {
     /**
      * <summary>
      * Create an mDNS scanner to discover local devices. Returned object must be kept alive while in use.
-     *
      * </summary>
      * <param name="subType"> the mDNS subtype to scan for: If the empty string, the mDNS subtype
      * `_nabto._udp.local` is located; if subtype is specified, `[subtype]._sub._nabto._udp.local` is located.</param>
@@ -79,8 +81,7 @@ public interface NabtoClient {
     /**
      * <summary>
      * Set a logger for getting log information from the SDK.
-     *
-     * </summary>
+      * </summary>
      * <param name="logger"> The logger to set.</param>
      */
     public void SetLogger(ILogger logger);
