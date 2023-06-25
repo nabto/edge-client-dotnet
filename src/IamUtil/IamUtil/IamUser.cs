735f5e46836c18cdbedec353cc7eba5deada79fa
Ulrik Gammelby <ug@nabto.com>
2023-06-25 12:36:28 +0200

    add missing IamUtil documentation

--- src/IamUtil/IamUtil/IamUser.cs
+++ src/IamUtil/IamUtil/IamUser.cs
@@ -1,25 +1,61 @@
 namespace Nabto.Edge.ClientIam;
 
+/**
+ * <summary>This class represents <see href="https://docs.nabto.com/developer/guides/push/intro.html">Nabto Push FCM</see> configuration.</summary>
+ */
 public class Fcm
 {
+    /**
+     * <summary>FCM registration token for push notifications, identifying a specific client to receive notifications. See the <see href="https://docs.nabto.com/developer/guides/push/intro.html">Nabto Edge Push Intro</see>.</summary>
+     */
     public string? Token { get; set; }
+    /**
+     * <summary>FCM project id push notifications. See the <see href="https://docs.nabto.com/developer/guides/push/intro.html">Nabto Edge Push Intro</see>.</summary>
+     */
     public string? ProjectId { get; set; }
 }
 
+/**
+ * <summary>This class represents a Nabto Edge IAM user.</summary>
+ */
 public class IamUser
 {
+    /**
+     * <summary>Username of the user.</summary>
+     */
     public string? Username { get; set; }
+    /**
+     * <summary>Display name of the user.</summary>
+     */
     public string? DisplayName { get; set; }
 
+    /**
+     * <summary>Public key fingerprint of the user.</summary>
+     */
     public string? Fingerprint { get; set; }
 
+    /**
+     * <summary>Password for the user (only for PUT operations).</summary>
+     */
     public string? Password { get; set; }
 
+    /**
+     * <summary>Server Connect Token for user.</summary>
+     */
     public string? Sct { get; set; }
 
+    /**
+     * <summary>The single role this user is assigned.</summary>
+     */    
     public string? Role { get; set; }
 
+    /**
+     * <summary>The <see cref="Fcm"/> configuration for the user.</summary>
+     */ 
     public Fcm? Fcm { get; set; }
 
+    /**
+     * <summary>The push notification categories the user is subscribed to.</summary>
+     */ 
     public List<string>? NotificationCategories { get; set; }
 }
--- src/IamUtil/IamUtil/IamUtil.cs
+++ src/IamUtil/IamUtil/IamUtil.cs
@@ -2,6 +2,23 @@
 namespace Nabto.Edge.ClientIam;
 using Nabto.Edge.ClientIam.Impl;
 
+
+/**
+ * <summary>
+ * <para>This class simplifies interaction with the Nabto Edge Embedded SDK device's CoAP IAM endpoints.</para>
+ *
+ * <para>For instance, it is made simple to invoke the different pairing endpoints - just invoke a simple high level
+ * pairing function to pair the client with the connected device and don't worry about CBOR encoding and decoding.</para>
+ *
+ * <para>Read more about the important concept of pairing in the <see href="https://docs.nabto.com/developer/guides/concepts/iam/pairing.html">Nabto Edge IAM Pairing</see> guide.</para>
+ *
+ * <para>All the most popular IAM device endpoints are wrapped to also allow management of the user profile on the device
+ * (own or other users' if client is in admin role).</para>
+ *
+ * <para>Note that the device's IAM configuration must allow invocation of the different functions and the pairing modes must
+ * be enabled at runtime. Read more about that in the <see href="https://docs.nabto.com/developer/guides/concepts/iam/intro.html">general Nabto Edge IAM intro</see>.</para>
+ * </summary>
+ */
 public class IamUtil
 {
 
@@ -11,6 +28,24 @@ public class IamUtil
         return Pairing.PairLocalInitialAsync(connection);
     }
 
+    /**
+     * <summary>
+     * <para>Perform <see href="https://docs.nabto.com/developer/guides/concepts/iam/pairing.html#open-local">Local Open pairing</see>, requesting the specified username.</para>
+     *
+     * <para>Local open pairing uses the trusted local network (LAN) pairing mechanism. No password is required for pairing and no
+     * invitation is needed, anybody on the LAN can initiate pairing.</para>
+     *
+     * <param name="connection">An established connection to the device this client should be paired with</param>
+     * <param name="desiredUsername">Assign this username on the device if available (pairing fails with .USERNAME_EXISTS if not)</param>
+     *
+     * <exception cref="IamException">Thrown with <see cref="IamError"/>`USERNAME_EXISTS` if the username and/or password is invalid.</exception>
+     * <exception cref="IamException">Thrown with <see cref="IamError"/>`INVALID_INPUT` if desiredUsername is <see href="https://docs.nabto.com/developer/api-reference/coap/iam/post-users.html#request">not valid</see>.</exception>
+     * <exception cref="IamException">Thrown with <see cref="IamError"/>`BLOCKED_BY_DEVICE_CONFIGURATION` if the device configuration does not support local open pairing (the `IAM:PairingLocalOpen` action
+     * is not set for the Unpaired role or the device does not support the pairing mode at all).</exception>
+     * <exception cref="IamException">Thrown with <see cref="IamError"/>`PAIRING_MODE_DISABLED` if the pairing mode is configured on the device but is disabled at runtime.</exception>
+     * <exception cref="IamException">Thrown with <see cref="IamError"/>`IAM_NOT_SUPPORTED` if Nabto Edge IAM is not supported by the device.</exception>
+     * </summary>
+     */
     public static Task PairLocalOpenAsync(Nabto.Edge.Client.Connection connection, string desiredUsername)
     {
         return Pairing.PairLocalOpenAsync(connection, desiredUsername);
